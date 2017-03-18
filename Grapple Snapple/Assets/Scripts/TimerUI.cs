dccb285b922240b45df0f2e85eb1dfaacaeb615a
Kyle Gilbert <kyle.gilbert1622@gmail.com>
2017-03-17 23:01:38 -0700

    Added to map

---
+++ Grapple Snapple/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour {
+
+    public Text timerText;
+
+    public float seconds;
+    public float minutes;
+
+	void Start () {
+        timerText = GetComponent<Text>() as Text;
+	}
+	
+	void Update () {
+        minutes = (int) (Time.timeSinceLevelLoad / 60f);
+        seconds = (int) (Time.timeSinceLevelLoad % 60f);
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+}
